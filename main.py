bb249ee71a32971ae1f5c3cf5aa24bf4d4865e37
Sanjith Chockan <sanjith.chockan@gmail.com>
2024-04-18 19:19:39 -0500

    implemented main chat loop

---
+++ main.py
@@ -0,0 +1,27 @@
+
+bot_name = "bot"
+
+def get_user():
+    global name
+    print(f'{bot_name}: Hello! What is your name?')
+    name = input()
+
+def respond():
+    return ""
+
+def chatbot_loop():
+    while True:
+        # user input
+        user_input = input(f'{name}: ')
+
+        if user_input == 'quit':
+            # save info before quitting
+            break
+        # bot response
+        bot_response = respond()
+        print(f'{bot_name}: {bot_response}') 
+    print(f'{bot_name}: Have a nice day!')
+
+if __name__ == "__main__":
+    get_user()
+    chatbot_loop()
